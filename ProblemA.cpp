17851880bd115ad9f3489cd826dba1fd7f0c961a
Filiberto Fuentes <pockeetgym@gmail.com>
2019-06-17 13:01:26 -0500

    Create ProblemA.cpp

---
+++ ProblemA.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define forn(i, n) for(int i=0;i<n;i++)
+#define lli long long int
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define psi pair<int,pii>
+#define pb push_back
+#define vi vector<int>
+#define vii vector< vi >
+
+using namespace std;
+
+const int MAXN = 1000001;
+
+int cont[26];
+
+// чтение и перевод это будет пустой тратой времени
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	string cad;
+	cin>>cad;
+	forn(i, cad.size()) cont[cad[i] - 'a']++;
+	forn(i, 26)
+		while(cont[i]--) cout<<char(i + 'a');
+	cout<<endl;
+	return 0;
+}
